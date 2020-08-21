acecd3f5c380ca2c0c57c061fa47c58348bdcc8f
Erica Lopes <33881813+ericaolopess@users.noreply.github.com>
2020-08-20 21:54:42 -0300

    aula 2

---
+++ NaveSI/nave.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+var a = 2
+var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_debug("Ola Mundo!!!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print_debug("FPS")
