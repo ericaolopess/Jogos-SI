d783923c27f4b15ab445c3d2f9a7d801bce23030
Erica Lopes <33881813+ericaolopess@users.noreply.github.com>
2020-10-01 21:53:01 -0300

    pular, vida, morrer, plataforma

--- AventurasLili/Script/Player.gd
+++ AventurasLili/Script/Player.gd
@@ -1,6 +1,24 @@
 extends KinematicBody2D
 
-var MOVE_SPEED = 200
+
+
+const MOVE_SPEED = 200
+const JUMP_FORCE = 1100
+const GRAVITY = 50
+const MAX_FALL_SPEED = 500
+
+var y_velo = 0
+
+var scene_atual
+var pontos = 0
+
+func _ready():
+	scene_atual = get_tree().current_scene
+
+func count_point():
+	pontos += 1
+	scene_atual.mostra_pontos(pontos)
+	
 
 func _physics_process(delta):
 	var move_dir = 0
@@ -16,7 +34,22 @@ func _physics_process(delta):
 	else:
 		$sprite.animation = "idle"
 		
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, 0),Vector2(0,-1))
+	var no_chao = $RayD.is_colliding() or $RayE.is_colliding()
+	
+	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo),Vector2(0,-1))
+	
+	var grounded = is_on_floor()
+	y_velo += GRAVITY
+	
+	if grounded and Input.is_action_pressed("ui_up"):
+		y_velo = -JUMP_FORCE
+	
+	if grounded and y_velo >= 0:
+		y_velo = 5
+		
+	if y_velo > MAX_FALL_SPEED:
+		y_velo = MAX_FALL_SPEED	
+	
 		
 
 
